299e356d9cd49b12bfa1fbec5570a69ee3648aac
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-26 13:46:26 +0900

    refactor: 세션 이름을 상수로 관리

--- src/main/java/codesquad/springcafe/interceptor/LoginCheckInterceptor.java
+++ src/main/java/codesquad/springcafe/interceptor/LoginCheckInterceptor.java
@@ -7,12 +7,14 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.io.IOException;
 
+import static codesquad.springcafe.controller.LoginController.LOGIN_SESSION_NAME;
+
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         HttpSession session = request.getSession(false);
-        if(session == null || session.getAttribute("springCafeMember") == null){
+        if(session == null || session.getAttribute(LOGIN_SESSION_NAME) == null){
             response.sendRedirect("/login?redirectURL=" + request.getRequestURI());
             return false;
         }
